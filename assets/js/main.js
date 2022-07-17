aca2be1629aa152dee3c071ff1b90646436d1282
Higor Costa <higorcosta972@gmail.com>
2022-07-17 11:35:45 -0300

    Reposiciona o if else

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,8 +24,9 @@ function checaInputs(cidade, uf, rua) {
         $('.erro-cidade').css({display: 'block'});
     } else if (uf == '') {
         $('.erro-uf').css({display: 'block'});
-    } else if (rua == '') {
-        $('.erro-rua').css({display: 'block'});
+    } 
+    else if (rua == '') {
+         $('.erro-rua').css({display: 'block'});
     }
 }
 
